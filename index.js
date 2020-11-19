f2bf778a39b0f2934bb579dae8b0ca1fe1517216
George A. A. Bonespírito <torgge@gmail.com>
2020-11-19 11:12:05 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const relations = ['costo', 'costo.mercaderia', 'costo.mercaderia.unidad']
+
+function buildQueryJoins(str) {
+    let tables = [];
+    return str
+        .split('.')
+        .reduce(function (joinArray, table, index) {
+            // console.log(table, index, index % 2)
+            joinArray.push(
+                (index > 0)
+                    ? { join: `${tables[index - 1]}.${table}`, table: table }
+                    : { join: `c.${table}`, table: table }
+            )
+            tables.push(table)
+            return joinArray
+        }, []);
+}
+
+function relationsToQueryBuilderJoins(relations) {
+    let array = []
+    let result = [];
+    const map = new Map()
+    relations.map(rel => array = array.concat(buildQueryJoins(rel)))
+    for (const item of array) {
+        if (!map.has(item.join)) {
+            map.set(item.join, true);
+            result.push({
+                join: item.join,
+                table: item.table
+            });
+        }
+    }
+    return result;
+}
+
+console.log(relationsToQueryBuilderJoins(relations))
+
